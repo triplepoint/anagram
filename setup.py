f9566eb0cb321de23f415734fa2d2886648de4b8
Jonathan Hanson <jonathan@jonathan-hanson.org>
2015-03-20 00:45:18 -0700

    Initial commit; project description and setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'Anagram',
+    'description': 'A library for finding anagrams in a list of words.',
+    'author': 'Jonathan Hanson',
+    'download_url': '@TODO - github repo',
+    'author_email': 'jonathan@jonathan-hanson.org',
+    'version': '0.1',
+    'install_requires': [],
+    'packages': [
+        'anagram'
+    ],
+}
+
+setup(**config)
